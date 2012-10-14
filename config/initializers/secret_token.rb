997f2227c294a05562be5c85f22e1321d3e74068
myowin76 <myowin76@gmail.com>
2012-10-14 13:13:09 +0100

    Initial rails 3, rspec installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snapshot::Application.config.secret_token = '488ae6bbc44197403842d0227e40283b6135bfc887737c7386183657731ad00b7abb9ac0d0ee432ea12693c225010a1557750f21f74e623c9b99e513f703833c'
