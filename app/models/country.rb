c87f99d620f29cf2b3baf8c5e4df98e9e325a92d
myowin76 <myowin76@gmail.com>
2012-10-24 16:14:36 +0100

    minor ignore

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,4 +2,5 @@ class Country < ActiveRecord::Base
   attr_accessible :name, :desc
 
   has_many :stores
+  has_many :locations
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121022211513) do
+ActiveRecord::Schema.define(:version => 20121024143402) do
 
   create_table "audits", :force => true do |t|
     t.integer  "store_id"
@@ -62,6 +62,16 @@ ActiveRecord::Schema.define(:version => 20121022211513) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  create_table "locations", :force => true do |t|
+    t.string   "name"
+    t.integer  "country_id"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "locations", ["country_id"], :name => "index_locations_on_country_id"
 
   create_table "media_locations", :force => true do |t|
     t.string   "name"
