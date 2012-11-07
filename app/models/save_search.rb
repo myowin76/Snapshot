e940a46feed76d13f0b99b7f353c27cb48a254f4
myowin76 <myowin76@gmail.com>
2012-11-07 11:53:38 +0000

    style - scss updates minor

--- app/models/save_search.rb
+++ app/models/save_search.rb
@@ -2,6 +2,8 @@ class SaveSearch < ActiveRecord::Base
   belongs_to :user
   attr_accessible :name, :params, :user_id 
 
+  validates :name, :presence => {:message => 'Please give a name!'}
+
   def self.saved_searches user
 		find_all_by_user_id(user.id)  	
   end
