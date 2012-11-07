95bfc9aa065dec718d23449fa684220fc5391125
myowin76 <myowin76@gmail.com>
2012-11-07 18:03:21 +0000

    style - minor tab fix

--- app/assets/javascripts/save_searches.js
+++ app/assets/javascripts/save_searches.js
@@ -1,11 +1,11 @@
 $(function () {
   //$('.edit_task input[type=submit]').remove();
   $('#new_save_search .cancel').click(function () {
-    alert("hi");
+    
     return false;
-  }); 
+  });
+
 	$('#new_save_search').live('submit', function () {
-	    
 	    $('#save_search_params').val($('#search_form').serialize()); 
 
 	    
