699eca64f8b71f335a98f03d6b7c2e4ff0792a55
myowin76 <myowin76@gmail.com>
2012-12-03 14:42:24 +0000

    data-structures sql

--- db/migrate/20121129220217_data_structure_changes.rb
+++ db/migrate/20121129220217_data_structure_changes.rb
@@ -1,36 +1,36 @@
 class DataStructureChanges < ActiveRecord::Migration
   def up
 
-  	# execute(%{
-   #    INSERT INTO brandings (photo_id, brand_id) 
-			# 	SELECT id, brand_id FROM photos
-	  # })
-	  # execute(%{
-   #    INSERT INTO categorizations (photo_id, category_id) 
-			# 	SELECT id, category_id FROM photos
-	  # })
-	  # execute(%{
-   #    INSERT INTO medialocations (photo_id, media_location_id) 
-			# 	SELECT id, media_location_id FROM photos
-	  # })
-	  # execute(%{
-   #    INSERT INTO mediavehicles (photo_id, media_vehicle_id) 
-			# 	SELECT id, media_vehicle_id FROM photos
-	  # })
-	  # execute(%{
-   #    INSERT INTO mediatypes (photo_id, media_type_id) 
-			# 	SELECT id, media_type_id FROM photos
-	  # })
-	  # execute(%{
-   #    INSERT INTO promotiontypes (photo_id, promotion_type_id) 
-			# 	SELECT id, promotion_type_id FROM photos
-	  # })
-	  # remove_column :photos, :brand_id
-	  # remove_column :photos, :category_id
-	  # remove_column :photos, :media_location_id
-	  # remove_column :photos, :media_vehicle_id
-	  # remove_column :photos, :media_type_id
-	  # remove_column :photos, :promotion_type_id
+  	execute(%{
+      INSERT INTO brandings (photo_id, brand_id) 
+				SELECT id, brand_id FROM photos
+	  })
+	  execute(%{
+      INSERT INTO categorizations (photo_id, category_id) 
+				SELECT id, category_id FROM photos
+	  })
+	  execute(%{
+      INSERT INTO medialocations (photo_id, media_location_id) 
+				SELECT id, media_location_id FROM photos
+	  })
+	  execute(%{
+      INSERT INTO mediavehicles (photo_id, media_vehicle_id) 
+				SELECT id, media_vehicle_id FROM photos
+	  })
+	  execute(%{
+      INSERT INTO mediatypes (photo_id, media_type_id) 
+				SELECT id, media_type_id FROM photos
+	  })
+	  execute(%{
+      INSERT INTO promotiontypes (photo_id, promotion_type_id) 
+				SELECT id, promotion_type_id FROM photos
+	  })
+	  remove_column :photos, :brand_id
+	  remove_column :photos, :category_id
+	  remove_column :photos, :media_location_id
+	  remove_column :photos, :media_vehicle_id
+	  remove_column :photos, :media_type_id
+	  remove_column :photos, :promotion_type_id
 	  
   end
 
