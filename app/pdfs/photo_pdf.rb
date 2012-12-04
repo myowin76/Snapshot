4211cc4a48d493be024b94423f05cda272f51fde
myowin76 <myowin76@gmail.com>
2012-12-04 15:32:03 +0000

    minor - pdf fix

--- app/pdfs/photo_pdf.rb
+++ app/pdfs/photo_pdf.rb
@@ -3,36 +3,61 @@ class	PhotoPdf < Prawn::Document
 		super()
 		@photo = photo
 		photo_image
+		move_down(30)
 		photo_details
 
-		move_down(30)
+		
 
 		move_down(10)
 
 	end
 	def photo_image
 		text "File Name: #{@photo.photo_file_name}", :size => 14, :style => :bold
-		
 		image open("#{@photo.photo.url(:medium).to_s.sub!(/\?.+\Z/, '')}")
 
 	end
+
 	def photo_details
-		@categories 
-
-		text "Category: #{@photo.categories.first.name}", :size => 12
-		text "Brand: #{@photo.brands.first.name}", :size => 12
-		text "Store: #{@photo.audit.store.name}", :size => 12
 		text "Retailer: #{@photo.audit.retailer.name}", :size => 12
-
-		text "Promotion Calendar: #{@photo.promotion_calendar.name unless @photo.promotion_calendar_id.nil? }"
-		text "Promotion Type: #{@photo.promotion_types.first.name if @photo.promotion_types }"
-		text "Media Location: #{@photo.media_locations.first.name  if @photo.media_locations }"
-		text "Media Type: #{@photo.media_types.first.name if @photo.media_types }"
+		text "Store: #{@photo.audit.store.name}", :size => 12
+		text "Store Format: #{@photo.audit.store.store_format.name}"
 		text "Sector: #{@photo.audit.store.retailer.sector.name}"
 		text "Environment Type: #{@photo.audit.environment_type.name}"
-		text "Media Vehicle: #{@photo.media_vehicles.first.name if @photo.media_vehicles }"
 		text "Country: #{@photo.audit.store.country.name}"
-		text "Store Format: #{@photo.audit.store.store_format.name}"
+		
+
+		if @photo.categories
+			@photo.categories.each do |cat|
+				text "Category: #{cat.name}", :size => 12
+			end
+		end
+		if @photo.brands
+			@photo.brands.each do |brand|
+				text "Brand: #{brand.name}", :size => 12
+			end
+		end
+
+		text "Promotion Calendar: #{@photo.promotion_calendar.name unless @photo.promotion_calendar_id.nil? }"
+		if @photo.promotion_types
+			@photo.promotion_types.each do |pt|
+				text "Promotion Type: #{pt.name}"
+			end
+		end
+		if @photo.media_locations
+			@photo.media_locations.each do |ml|		
+				text "Media Location: #{ml.name }"
+			end
+		end
+		if @photo.media_types
+			@photo.media_types.each do |mt|		
+				text "Media Type: #{mt.name}"
+			end
+		end		
+		if @photo.media_vehicles
+			@photo.media_vehicles.each do |mv|		
+				text "Media Vehicle: #{mv.name}"
+			end
+		end		
 		
 	end
 end
