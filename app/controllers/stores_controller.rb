182bee2244c462c481a89e4d55412a0e5c63f28b
myowin76 <myowin76@gmail.com>
2013-01-09 13:27:53 +0000

    bug fixing sql

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -147,7 +147,7 @@ class StoresController < ApplicationController
       @photo_categories = @store.photos.select("photos.id, audit_id, photo_file_name")
         .joins(:categories)
         .where("category_id in (?)", @selected_categories.map(&:id))
-        .group("category_id");
+        .group("categorizations.category_id");
         # .having("count(photo_id)");
         debugger
       @audits = @store.audits.order('created_at DESC')
