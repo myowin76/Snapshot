ea9184beb2ef52f5d361341f95dd1e0161efa849
myowin76 <myowin76@gmail.com>
2013-01-09 14:20:21 +0000

    bug fix test sql postgres heroku

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -147,7 +147,7 @@ class StoresController < ApplicationController
       @photo_categories = @store.photos.select("photos.id, audit_id, photo_file_name")
         .joins(:categories)
         .where("category_id in (?)", @selected_categories.map(&:id))
-        .group("category_id");
+        .group("category_id").order("category_id");
         # .having("count(photo_id)");
         
       @audits = @store.audits.order('created_at DESC')
