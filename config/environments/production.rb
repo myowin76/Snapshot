8a2a99d306f7f96c1fe4ebfda364690b021d02ef
myowin76 <myowin76@gmail.com>
2013-02-27 11:49:51 +0000

    production.rb action mailer host amended

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ Snapshot::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => "portal.snapshotworldwide.com" }
+  config.action_mailer.default_url_options = { :host => "snapshot-staging.herokuapp.com" }
 
 end
