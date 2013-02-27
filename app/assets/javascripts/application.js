0766ee6a72428ae79cdb2543dcd2a1b2e786ce3c
Dilraj Ahdan <dilrajahdan@gmail.com>
2013-02-27 16:22:47 +0000

    sorted filter labels once search is pressed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -517,58 +517,144 @@ function fixPosElement(el){
   });
 }
 
-//  Filter UI object
+
+
+//  Filter UI object: used for checkboxes only at the moment
 // ============================================
 var filterUI = {
 
-  init : function(){
+  init : function(el){
     "use strict";
 
     var that = this,
-        $filters = $('#search_form');
+        $filter = $(el);
 
-    $filters.on('change', 'input', function(){
+    that.addLabelsForExistingCheckbox($filter);
+    that.attachEventToCheckbox($filter);
+  },
 
-      if ( $(this).attr('checked') !== undefined ){
+
+  /*
+  el:
+  */
+  addLabelsForExistingCheckbox : function(el){
+
+    var that = this,
+        $checkedCheckboxes = $(el).find('input:checked');
+
+    $checkedCheckboxes.each(function(){
+      that.addLabel($(this));
+    });
+
+  },
+
+  /*
+  el: expect #filter - jQ obj
+  */
+  attachEventToCheckbox : function(el){
+
+    var that = this,
+        $filter = el;
+
+    //Set event listener
+    $filter.on('change', 'input[type="checkbox"]', function(){
+
+      if ($(this).attr('checked') !== undefined ){
         that.addLabel(this);
       }
       else{
         that.removeLabel(this);
       }
+    });// filter
 
-    });
   },
 
-  addLabel : function(el){
-    "use strict";
 
-    var $el = $(el);
+  /*
+  el : expect input
+  return : location desc
+  */
+  getLabelDesc : function(el){
 
-   //console.log($el.parents('.accordion-group').find('.accordion-heading').find('.accordion-toggle') );
-   //console.log($el.parent().text());
-    if (! $el.parents('.accordion-group').find('.accordion-heading').find('.labels').length ) {
-      $el.parents('.accordion-group').find('.accordion-heading')
-      .append('<div class="labels" />');
+
+
+    var elType = el.prop("tagName").toLowerCase();
+
+    // If el === input, what is the type
+    if (elType === "input") {
+      elType = el.attr('type');
     };
 
 
-    $el.parents('.accordion-group').find('.accordion-heading').find('.labels')
-    .append('<a data-id="' +
-            $el.attr('id') +
-            '" title="' + jQuery.trim($el.parent().text()) +'" class="label"><i class="icon-white icon-remove"></i>' +
-            $el.parent().text() +
-            '</a>');
+    switch (elType) {
+      case "select":
+        //console.log('This is a select', el.find(':selected').attr('data-desc') );
+        return $(el).find(':selected').attr('data-desc');
+        break;
+      case "checkbox":
+        console.log('This is a checkbox', $(el).attr('data-desc'),el );
+        return $(el).attr('data-desc');
+        break;
+      case "text":
+        //console.log('This is a textbox', el);
+        return $(el).val();
+        break;
+      default:
+        console.log('No element type found.');
+    }// switch
 
+  },
+
+
+  /*
+  el: expect input[type=check] ***:DA TODO check for input type = checkbox
+  */
+  addLabel : function(el){
+    "use strict";
+
+    //console.log('el', el);
+
+    var that = this,
+        $el = $(el),
+        $labels = $el.parents('.accordion-group')
+                  .find('.accordion-heading')
+                  .find('.filter-tag-wrapper');
+
+    //console.log('test ', $el.parents('.accordion-group').find('.accordion-heading').find('.filter-tag-wrapper') );
+
+    var dataDesc = that.getLabelDesc($el);
+
+    //console.log('dataDesc', dataDesc);
+    //console.log('dataDesc2', that.getLabelDesc($el) );
+
+    //console.log('addlabel desc', dataDesc );
+
+    // Add labels wrapper if it doesnt exist. ***TODO: Append only once fool
+    if (! $labels.length ) {
+      $el.parents('.accordion-group').find('.accordion-heading')
+      .append('<div class="filter-tag-wrapper" />');
+    };
+
+    // Append label to labels
+    $el.parents('.accordion-group')
+      .find('.accordion-heading')
+      .find('.filter-tag-wrapper')
+      .append('<a data-id="' +
+              $el.attr('id') +
+              '" title="' + dataDesc +'" class="filter-tag"><i class="icon-white icon-remove"></i>' +
+              dataDesc +
+              '</a>');
 
       var $label = $el.parents('.accordion-group').find('.accordion-heading')
                   .find('[data-id="'+ $el.attr('id')  +'"]');
 
-      console.log('label', $label);
-
       this.attachEventToLabel($label);
-
   },
 
+  /*
+    except:
+    el:
+  */
   removeLabel : function(el){
     "use strict";
 
@@ -577,35 +663,41 @@ var filterUI = {
     $el.parents('.accordion-group')
     .find('.accordion-heading')
       .find('[data-id="'+ $el.attr('id')  +'"]')
-      .remove();
-
+      .fadeOut(300, function(){
+        $(this).remove();
+      });
 
   },
 
+
   attachEventToLabel : function(el){
+    "use strict";
+
     var that = this,
         $el = $(el);
 
-    console.log('attach',$el);
+    //console.log('attach',$el);
 
     $el.parent().delegate("a", "click", function() {
       that.removeLabel( '#' + $(this).attr('data-id') );
       that.uncheckCheckbox( '#' + $(this).attr('data-id') );
-      
     });
 
   },
 
   uncheckCheckbox : function(el){
+    "use strict";
 
     var $el = $(el);
     $el.attr('checked', false);
 
   }
 
+  // uncheck
 
 };
 
+// ============================================
 
 
 
@@ -632,6 +724,9 @@ function initLazyLoad(el){
 }
 
 jQuery(document).ready(function($) {
+  
+  filterUI.init('#search_form');
+
 
   //initLazyLoad("img.lazy");
   /*
@@ -648,12 +743,3 @@ jQuery(document).ready(function($) {
 */
   
 });
-
-/*
-$(window).bind("load", function() { 
-    var timeout = setTimeout(function() {
-      $("img.lazy").trigger("sporty");
-      alert('yo');
-    }, 5000);
-}); 
-*/
