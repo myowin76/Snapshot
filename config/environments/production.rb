d312851d8a7359e3f261481699ea99d73d8a784b
myowin76 <myowin76@gmail.com>
2013-06-11 17:52:32 +0100

    devise sendgrid setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,13 @@ Snapshot::Application.configure do
 
   config.action_mailer.default_url_options = { :host => "portal.snapshotworldwide.com" }
 
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
+
 end
