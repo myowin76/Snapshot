ea8e4dd8bb09db803ad3f68cf1296546ff3bc770
myowin76 <myowin76@gmail.com>
2013-06-11 17:21:58 +0100

    sendgrid settings added

--- config/environment.rb
+++ config/environment.rb
@@ -9,13 +9,23 @@ Time::DATE_FORMATS[:default] = "%d/%m/%Y"
 
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.gmail.com',
-  :port => 587,
-  :domain => 'skyron.co.uk',
-  :authentication => "plain",
-  :user_name => 'aung.win@skyron.co.uk',
-  :password => 'mandalay7'
+#   ActionMailer::Base.smtp_settings = {
+#   :address => 'smtp.gmail.com',
+#   :port => 587,
+#   :domain => 'skyron.co.uk',
+#   :authentication => "plain",
+#   :user_name => 'aung.win@skyron.co.uk',
+#   :password => 'mandalay7'
+# }
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'app9715126@heroku.com',
+  :password       => 'aqkl4vjb',
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
 
 
