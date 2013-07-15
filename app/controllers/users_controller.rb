9a2760bc2537d60cb9a88f6f56c9e04615ec05ef
myowin76 <myowin76@gmail.com>
2013-07-15 13:09:40 +0100

    users controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # class UsersController < ApplicationController
 class UsersController < ApplicationController
   
-  include Devise::Controllers::Helpers
+  # include Devise::Controllers::Helpers
   before_filter :get_user, :only => [:index,:new,:edit]
   before_filter :accessible_roles, :only => [:new, :edit, :show, :update, :create]
   load_and_authorize_resource :only => [:index, :show, :new, :destroy, :edit, :update]
@@ -18,13 +18,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def new
-    respond_to do |format|
-      format.json { render :json => @user }   
-      format.xml  { render :xml => @user }
-      format.html
-    end
-  end
+
  
   def show
     respond_to do |format|
@@ -47,7 +41,19 @@ class UsersController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     respond_to_not_found(:json, :xml, :html)
   end
- 
+
+  def new
+
+    @user = User.new
+    
+    respond_to do |format|
+      format.json { render :json => @user }   
+      format.xml  { render :xml => @user }
+      format.html
+    end
+    
+  end
+
   def destroy
     @user.destroy!
  
@@ -62,7 +68,9 @@ class UsersController < ApplicationController
   end
  
   def create
+    debugger
     @user = User.new(params[:user])
+    
  
     if @user.save
       respond_to do |format|
@@ -77,6 +85,10 @@ class UsersController < ApplicationController
         format.html { render :action => :new, :status => :unprocessable_entity }
       end
     end
+  end
+
+  def create_user
+    debugger
   end
 
    # Get roles accessible by the current user
